9392bc7c167416aed11ac8a8be6c98bcf7e86037
Víctor Santiago <victorsantiago.matesub@gmail.com>
2022-06-30 18:15:45 +0200

    First Commit

---
+++ src/components/MyForm.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <p>{{ name }}</p>
+        <input ref="el" type="text" placeholder="Name" v-model="name"/>
+        <div>
+            <button 
+            :disabled="name.length == 0" 
+            @click="submitForm"
+            >
+            Submit
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue'
+
+export default {
+    setup() {
+        const name = ref('')
+        const el = ref()
+        const submitForm = () => {
+            console.log(`Form submitted! Name = ${name.value}`)
+        }
+
+        onMounted(() => {
+            el.value.focus()
+        })
+        return {
+            name,
+            el,
+            submitForm,
+        }
+    }
+}
+</script>
+
+<style>
+    input[type='text'] {
+        padding: 5px
+    }
+
+    button {
+        margin-top: 20px;
+    }
+</style>
